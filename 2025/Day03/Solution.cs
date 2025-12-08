4ae0b6f2e4d7ed71c3474923fa47b71b1de9f833
Viktor Johansson <discovicke@gmail.com>
2025-12-08 23:00:03 +0100

    day four done

--- 2025/Day03/Solution.cs
+++ 2025/Day03/Solution.cs
@@ -12,10 +12,42 @@ using System.Numerics;
 class Solution : Solver {
 
     public object PartOne(string input) {
-        return 0;
+        long power = 0;
+        foreach (var battery in input.Split("\n"))
+        {
+            var digits = LargestFixedOrder(battery, 2);
+            var maxValue = long.Parse(digits);
+            power += maxValue;
+        }
+        return power;
     }
 
-    public object PartTwo(string input) {
-        return 0;
+    public object PartTwo(string input) { 
+        long power = 0;
+        foreach (var battery in input.Split("\n"))
+        {
+            var digits = LargestFixedOrder(battery, 12);
+            var maxValue = long.Parse(digits);
+            power += maxValue;
+        }
+        return power;
+    }
+    private static string LargestFixedOrder(string s, int k)
+    {
+        long n = s.Length;
+        var toRemove = n - k;          // antal vi måste ta bort
+        var result = new List<char>();
+        foreach (var digit in s)
+        {                                   // Ta bort siffror från result om: 
+            while (toRemove > 0             // 1. Vi har siffror kvar att ta bort (toRemove > 0)
+                   && result.Count > 0      // 2. Result inte är tom
+                   && result[^1] < digit)   // 3. Senaste siffran i result är mindre än nuvarande siffra
+            {
+                result.RemoveAt(result.Count - 1);
+                toRemove--;
+            }
+            result.Add(digit);
+        }
+        return new string(result.GetRange(0, k).ToArray());
     }
 }
--- 2025/Day04/Solution.cs
+++ 2025/Day04/Solution.cs
@@ -12,10 +12,106 @@ using System.Numerics;
 class Solution : Solver {
 
     public object PartOne(string input) {
-        return 0;
+        var rolls = input.Split("\n");
+        int height = rolls.Length;
+        int width  = rolls[0].Length;
+        char rollChar = '@';
+        var grid = new char[height, width];
+        for (int r = 0; r < height; r++)
+        {
+            for (int c = 0; c < width; c++)
+            {
+                grid[r, c] = rolls[r][c];
+            }
+        }
+        var deltas = new (int dx, int dy)[]
+        {
+            (-1,-1),(0,-1),(1,-1),
+            (-1, 0),       (1, 0),
+            (-1, 1),(0, 1),(1, 1)
+        };
+
+        int totalRemoved = RemoveForRounds(grid, rollChar, deltas, 1);
+        return totalRemoved;
     }
 
-    public object PartTwo(string input) {
-        return 0;
+    public object PartTwo(string input)
+    {
+        var rolls = input.Split("\n");
+        int height = rolls.Length;
+        int width  = rolls[0].Length;
+        char rollChar = '@';
+        var grid = new char[height, width];
+        for (int r = 0; r < height; r++)
+        {
+            for (int c = 0; c < width; c++)
+            {
+                grid[r, c] = rolls[r][c];
+            }
+        }
+        var deltas = new (int dx, int dy)[]
+        {
+            (-1,-1),(0,-1),(1,-1),
+            (-1, 0),       (1, 0),
+            (-1, 1),(0, 1),(1, 1)
+        };
+        int totalRemoved = RemoveUntilStable(grid, rollChar, deltas);
+        return totalRemoved;
+    }
+    int RemoveWeakRolls(char[,] grid, char rollChar, (int dx, int dy)[] deltas)
+    {
+        int height = grid.GetLength(0);
+        int width  = grid.GetLength(1);
+        var toRemove = new List<(int r, int c)>();
+        for (int r = 0; r < height; r++)
+        {
+            for (int c = 0; c < width; c++)
+            {
+                if (grid[r, c] != rollChar) continue;
+                int neighbors = 0;
+                foreach (var (dr, dc) in deltas)
+                {
+                    int nr = r + dr, nc = c + dc;
+                    if (nr >= 0 && nr < height && nc >= 0 && nc < width &&
+                        grid[nr, nc] == rollChar)
+                    {
+                        neighbors++;
+                    }
+                }
+                if (neighbors < 4)
+                    toRemove.Add((r, c));
+            }
+        }
+        foreach (var (r, c) in toRemove)
+            grid[r, c] = '.';
+        return toRemove.Count;
+    }
+    int RemoveForRounds(char[,] grid, char rollChar, (int dx, int dy)[] deltas, int rounds)
+    {
+        int total = 0;
+        for (int i = 0; i < rounds; i++)
+        {
+            int removed = RemoveWeakRolls(grid, rollChar, deltas);
+            total += removed;
+
+            if (removed == 0)
+                break;
+        }
+        return total;
+    }
+    int RemoveUntilStable(char[,] grid, char rollChar, (int dx, int dy)[] deltas)
+    {
+        int total = 0;
+
+        while (true)
+        {
+            int removed = RemoveWeakRolls(grid, rollChar, deltas);
+            if (removed == 0)
+                break;
+
+            total += removed;
+        }
+
+        return total;
     }
 }
